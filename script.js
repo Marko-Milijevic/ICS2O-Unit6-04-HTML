a14440d174d2451236e1c4c31684d27b6e10a824
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-22 23:38:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let baseA = 0
+let baseB = 0
+let height = 0
+
+document.getElementById('button').addEventListener('click', calculate)
+
+function calculate () {
+  baseA = document.getElementById('input').value
+  baseB = document.getElementById('input2').value
+  height = document.getElementById('input3').value
+  area = (parseInt(baseA) + parseInt(baseB)) / 2 * parseInt(height)
+  alert(area)
+}
